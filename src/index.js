6dcbdb9a56ea0daf0f91e61fd36cf6cbb647784a
Frederico Perpetuo <fredsp.ti@gmail.com>
2023-02-07 13:02:21 -0300

    Resolução desafio 2 trilha node Rocketseat 2021

--- src/index.js
+++ src/index.js
@@ -10,19 +10,66 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error: "User not found!"});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request;
+  if((user.pro === false) && (user.todos.length === 10)){
+    return response.status(403).json({error: "Should not be able to let user create a new todo when is not Pro and already have ten todo!"})
+  }
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
-}
+  const {username} = request.headers;
+  const {id} =  request.params;
+  
+  const user = users.find(user => user.username === username);
+  if(!user){
+    return response.status(404).json({error: "User not found!"});
+  }
+
+  const validateTodo = validate(id, 4);
+  if(!validateTodo){
+    return response.status(400).json({error: "Parameter is not a UUID!"});
+  }
+   
+  const todo = user.todos.find(todo => todo.id === id);
+
+    if(!todo){
+      return response.status(404).json({error: "Todo not found!"});
+    }
+
+    request.user = user;
+    request.todo = todo;
+
+    return next();
+  }
+
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user){
+    return response.status(404).json({error: "User not found!"});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
